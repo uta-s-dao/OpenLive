e0415a8b6c6a4ebed930980174c6b04c002d1151
uta-s-dao <saruwatari.yuta379@mail.kyutech.jp>
2025-07-02 10:27:12 +0900

    set favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,25 +18,21 @@ export const metadata = {
     "福岡最大の学生間音楽フェスティバル、オープンライブ。14大学から集結したバンドが一堂に会し、ライブパフォーマンスを披露。音楽と大学生の熱い情熱が交差する瞬間をお見逃しなく！",
   keywords:
     "オープンライブ, おーぷんらいぶ, 福岡, 大学生, 音楽フェスティバル, ライブ, バンド",
-  icons: {
-    icon: "/openlive.jpg",
-  },
-
   openGraph: {
     title: "オープンライブ",
     description:
       "福岡,北九州の14大学から集結したバンドが一堂に会し、ライブパフォーマンスを披露。",
-    url: "https://open-live.vercel.app", // 実際のイベントURLに差し替えてください
+    url: "https://openlive.cc", // 実際のイベントURLに差し替えてください
     siteName: "オープンライブ",
     images: [
       {
-        url: "//openlive.green.jpg", // public//openlive.green.jpgを指定
+        url: "//favicon.ico", // public//favicon.icoを指定
         width: 1200,
         height: 630,
         alt: "オープンライブ 福岡の大学生音楽フェスティバル",
       },
       {
-        url: "//openlive.green.jpg", // public//openlive.green.jpgを指定
+        url: "//favicon.ico", // public//favicon.icoを指定
         width: 1200,
         height: 1200,
         alt: "オープンライブ 福岡の大学生音楽フェスティバル",
