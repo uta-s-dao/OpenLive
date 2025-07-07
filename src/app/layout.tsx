cecdee1f64098777a4f507d98ccd31acb676ede6
uta-s-dao <saruwatari.yuta379@mail.kyutech.jp>
2025-07-07 23:07:36 +0900

    a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,7 @@
 import { ReactNode } from "react";
-import { Noto_Sans_JP } from "next/font/google";
 import ClientHeader from "./components/ClientHeader";
 import SocialIcon from "./components/SocialIcon";
 import { SOCIAL_URLS } from "./constants/urls";
-
-const notoSansJP = Noto_Sans_JP({
-  weight: ["400", "700"],
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-noto-sans-jp",
-});
 
 export const metadata = {
   metadataBase: new URL("https://openlive.cc"),
@@ -22,17 +14,17 @@ export const metadata = {
     title: "オープンライブ",
     description:
       "福岡,北九州の14大学から集結したバンドが一堂に会し、ライブパフォーマンスを披露。",
-    url: "https://openlive.cc", // 実際のイベントURLに差し替えてください
+    url: "https://openlive.cc",
     siteName: "オープンライブ",
     images: [
       {
-        url: "/openlive.green.jpg", // public//openlive.green.jpgを指定
+        url: "/openlive.green.jpg",
         width: 1200,
         height: 630,
         alt: "オープンライブ 福岡の大学生音楽フェスティバル",
       },
       {
-        url: "/openlive.green.jpg", // public//openlive.green.jpgを指定
+        url: "/openlive.green.jpg",
         width: 1200,
         height: 1200,
         alt: "オープンライブ 福岡の大学生音楽フェスティバル",
@@ -44,8 +36,8 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang='ja' className={`${notoSansJP.variable}`}>
-      <body className='min-h-screen font-noto'>
+    <html lang='ja'>
+      <body className='min-h-screen'>
         <ClientHeader />
         <main>{children}</main>
         <footer className='py-2 bg-white'>
